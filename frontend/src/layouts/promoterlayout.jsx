d0d966698a950fb08ffcb0e5fc2fd7320311bbf3
zyvrex1 <165166360+zyvrex1@users.noreply.github.com>
2026-02-28 23:32:51 +0800

    minimal update

--- frontend/src/layouts/promoterlayout.jsx
+++ frontend/src/layouts/promoterlayout.jsx
@@ -1,26 +1,4 @@
-// import { Outlet } from "react-router-dom";
-// import PromoterSidebar from "../promotercomponents/promotersidebar";
-// // import PromoterHeader from "../promotercomponents/header";
 
-// export default function PromoterLayout() {
-//   const currentUser = {
-//     name: "John Promoter",
-//     role: "Promoter"
-//   };
-
-//   return (
-//     <div style={{ display: "flex" }}>
-//       <PromoterSidebar />
-
-//       <div style={{ flex: 1 }}>
-//         <PromoterHeader user={currentUser} />
-//         <div style={{ padding: "20px" }}>
-//           <Outlet />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
 import { Outlet } from "react-router-dom";
 import PromoterSidebar from "../promotercomponents/promotersidebar.jsx";
