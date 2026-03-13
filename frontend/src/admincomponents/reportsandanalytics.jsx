4e2b1796bdf1ebc7ce96502e59c75d20ab6ac722
zyvrex1 <165166360+zyvrex1@users.noreply.github.com>
2026-03-14 04:17:01 +0800

    Minimal Update

--- frontend/src/admincomponents/reportsandanalytics.jsx
+++ frontend/src/admincomponents/reportsandanalytics.jsx
@@ -206,8 +206,8 @@ export default function ReportsAnalytics() {
                                     
                                     <defs>
                                         <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
-                                            <stop offset="5%" stopColor="#f1f5f9" stopOpacity={0.8}/>
-                                            <stop offset="95%" stopColor="#f1f5f9" stopOpacity={0}/>
+                                            <stop offset="5%" stopColor="#c62828" stopOpacity={0.8}/>
+                                            <stop offset="95%" stopColor="#f1bcbc" stopOpacity={0}/>
                                         </linearGradient>
                                     </defs>
 
